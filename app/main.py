d409762689ed1c86a0cc6834d59d62c99efe46bb
Shaayaan Sayed <ssayed@Shaayaans-MacBook-Pro.local>
2025-06-14 17:17:37 -0500

    add lubhna as user

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ async def get_users():
         "users": [
             {"id": 1, "name": "Alice", "email": "alice@example.com"},
             {"id": 2, "name": "Bob", "email": "bob@example.com"},
-            {"id": 3, "name": "Charlie", "email": "charlie@example.com"},
+            {"id": 3, "name": "Lubhna", "email": "charlie@example.com"},
         ]
     }
 
